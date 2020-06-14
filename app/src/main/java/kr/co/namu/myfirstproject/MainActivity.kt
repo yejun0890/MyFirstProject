5b5ffd3df0cfa9feb0a5699891134601abe2a556
yejun <yejun0890@gmail.com>
2020-06-14 18:20:44 +0900

    라디오 버큰 활용 연습 ->라디오 그룹으로 묶어두고, 버튼을 배치

---
+++ app/src/main/java/kr/co/namu/myfirstproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package kr.co.namu.myfirstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kr.co.namu.myfirstproject.R.layout.activity_main
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
